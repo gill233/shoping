c110abfff922ee78aeab2d48ac894e5b2374f673
gill zadji <gill233>
2022-11-15 09:39:28 +0100

    new product page

---
+++ pages/products.js
@@ -0,0 +1,21 @@
+const Products = ()=>{
+
+    return(
+        <>
+        <h1>Cool product 1</h1>
+        <p>This is a very cool product</p>
+        <p>€50</p>
+        <hr/>
+        <h1>Cool product 2</h1>
+        <p>This is another cool product</p>
+        <p>€77</p>
+        <hr/>
+        <h1>Cool product 3</h1>
+        <p>This is a super cool product</p>
+        <p>€150</p>
+        <hr/>
+        
+        </>
+    )
+}
+export default Products;
